acca0393db2e398235fc2d2e9ceefe631e6c795a
zioqh <117847487+zioqh@users.noreply.github.com>
2022-11-09 17:40:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 
 prefix = '.'
-client = discord.Bot(prefix=prefix, intents=discord.Intents.all())
+client = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
   print("\n\ngit bot is on!\n\n")
   
-client.run(yourtoken)
+client.run(yourtoken')
